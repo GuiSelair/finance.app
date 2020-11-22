89f839ff78644988100ac9a309e74e8caaa8e3fc
Guilherme Selair <guilherme.lima1997@hotmail.com>
2020-11-22 18:55:36 -0300

    feat: Add Expenses Table

---
+++ backend/src/modules/Expense/infra/typeorm/entities/Expense.ts
@@ -0,0 +1,66 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Card from '../../../../Card/infra/typeorm/entities/Card';
+import User from '../../../../User/infra/typeorm/entities/User';
+
+@Entity('expenses')
+class Expense {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  purchase_date: Date;
+
+  @CreateDateColumn()
+  due_date?: Date;
+
+  @Column()
+  amount: number;
+
+  @Column()
+  split_expense?: boolean;
+
+  @Column()
+  share_with?: string;
+
+  @Column()
+  percentage_of_each?: string;
+
+  @Column()
+  card_id: string;
+
+  @ManyToOne(() => Card)
+  @JoinColumn({ name: 'card_id' })
+  card: Card;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column()
+  parcel: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
+
+export default Expense;
