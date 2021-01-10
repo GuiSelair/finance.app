c992811d7677806b7e6f4e57ab5c92bdfd2826d8
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-01-10 16:00:47 -0300

    feat(apperror): added custom error capture

---
+++ backend/src/shared/errors/AppError.ts
@@ -0,0 +1,12 @@
+class AppError {
+  public readonly message;
+
+  public readonly statusCode;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default AppError;
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,11 +1,24 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
+import AppError from '../../errors/AppError';
 import routes from './routes';
 import '../typeorm';
 import '../../Container';
 
 const app = express();
+app.use(express.json());
+
 app.use(routes);
 
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+      type: 'error',
+    });
+  }
+});
+
+// eslint-disable-next-line no-console
 app.listen(3333, () => console.log('>> Server UP on port 3333'));
