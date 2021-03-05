5d533a27da46bfd3e1c0f6ce519c1b12f6cce9b4
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-03-04 21:50:27 -0300

    Feat: Added expensesMonthTable

--- backend/src/shared/infra/typeorm/migrations/1614728346768-CreateExpensesMonthTable.ts
+++ backend/src/shared/infra/typeorm/migrations/1614728346768-CreateExpensesMonthTable.ts
@@ -1,10 +1,11 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateCards1603067921591 implements MigrationInterface {
+export default class CreateExpensesMonthTable1614728346768
+  implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'cards',
+        name: 'expenses-month',
         columns: [
           {
             name: 'id',
@@ -14,20 +15,24 @@ export default class CreateCards1603067921591 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'name',
-            type: 'varchar',
-          },
-          {
-            name: 'flag',
-            type: 'varchar',
-          },
-          {
-            name: 'due_date',
-            type: 'timestamp',
-          },
-          {
-            name: 'user_id',
+            name: 'expense_id',
             type: 'uuid',
+          },
+          {
+            name: 'number_current_of_parcel',
+            type: 'integer',
+          },
+          {
+            name: 'number_total_of_parcel',
+            type: 'integer',
+          },
+          {
+            name: 'month',
+            type: 'integer',
+          },
+          {
+            name: 'value_of_parcel',
+            type: 'float',
           },
           {
             name: 'created_at',
@@ -42,11 +47,11 @@ export default class CreateCards1603067921591 implements MigrationInterface {
         ],
         foreignKeys: [
           {
-            name: 'cardUser',
-            columnNames: ['user_id'],
-            referencedTableName: 'users',
+            name: 'expenseMonth',
+            columnNames: ['expense_id'],
+            referencedTableName: 'expenses',
             referencedColumnNames: ['id'],
-            onDelete: 'SET NULL',
+            onDelete: 'CASCADE',
             onUpdate: 'CASCADE',
           },
         ],
@@ -55,6 +60,7 @@ export default class CreateCards1603067921591 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('cards');
+    await queryRunner.dropForeignKey('expense-month', 'expenseMonth');
+    await queryRunner.dropTable('expenses-month');
   }
 }
