c3f26d004aa08a8df70d26d5b3e3baf7e8feb963
Guilherme Selair <guilhermeselair@gmail.com>
2023-02-08 22:01:12 -0300

    Feat: Added Header component

--- frontend/src/components/shared/Header/index.tsx
+++ frontend/src/components/shared/Header/index.tsx
@@ -1,5 +1,69 @@
-import { HeaderContainer } from './styles';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Gear } from 'phosphor-react';
+
+import {
+	HeaderContainer,
+	Logo,
+	LinkToPage,
+	AddExpenseLink,
+	SystemOptions,
+} from './styles';
 
 export default function Header(): JSX.Element {
-	return <HeaderContainer></HeaderContainer>;
+	const { pathname } = useRouter();
+
+	const primaryPageSelected = pathname.split('/')[0];
+
+	return (
+		<HeaderContainer>
+			<div>
+				<Logo href="/">
+					<strong>Finance</strong>.app
+				</Logo>
+				<nav>
+					<LinkToPage
+						href="/"
+						prefetch={false}
+						active={primaryPageSelected === ''}
+					>
+						Dashboard
+					</LinkToPage>
+					<LinkToPage
+						href="/cards"
+						prefetch={false}
+						active={primaryPageSelected === 'cards'}
+					>
+						Cartões
+					</LinkToPage>
+					<LinkToPage
+						href="/division"
+						prefetch={false}
+						active={primaryPageSelected === 'division'}
+					>
+						Divisões
+					</LinkToPage>
+				</nav>
+			</div>
+			<div>
+				<AddExpenseLink href="/add/expenses" prefetch={false}>
+					Criar despesa
+				</AddExpenseLink>
+				<SystemOptions>
+					<Link href="/config">
+						<Gear size={24} />
+					</Link>
+					<button type="button">
+						<Image
+							src="http://www.github.com/guiselair.png"
+							alt=""
+							width={48}
+							height={48}
+						/>
+					</button>
+				</SystemOptions>
+			</div>
+		</HeaderContainer>
+	);
 }
