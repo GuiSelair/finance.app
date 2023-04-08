0ac4d5f82c4d5f392996f6353da37ef4a44cf17c
Guilherme Selair <guilhermeselair@gmail.com>
2023-04-08 15:29:54 -0300

    Feat: 🔨 Added base select component

---
+++ frontend/src/components/shared/Form/Select/index.tsx
@@ -0,0 +1,43 @@
+import { ComponentProps } from 'react';
+import ReactSelect, { OptionsOrGroups } from 'react-select';
+
+export interface SelectOptionProps {
+	label: string | number;
+	value: string | number;
+}
+
+export interface SelectGroupOptionsProps {
+	label: string;
+	options: SelectOptionProps[];
+}
+
+export type SelectOptions = OptionsOrGroups<
+	SelectOptionProps,
+	SelectGroupOptionsProps
+>;
+
+interface SelectProps extends ComponentProps<typeof ReactSelect> {
+	width?: string;
+	options: SelectOptions;
+}
+
+export function Select({ width, ...rest }: SelectProps) {
+	return (
+		<ReactSelect
+			styles={{
+				container(base, props) {
+					return {
+						...base,
+						width,
+					};
+				},
+				indicatorSeparator(base, props) {
+					return {
+						display: 'none',
+					};
+				},
+			}}
+			{...rest}
+		/>
+	);
+}
