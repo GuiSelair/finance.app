d213ac501be8669183783d68f3b4d272870eafa8
Guilherme Selair <guilhermeselair@gmail.com>
2023-04-17 22:03:57 -0300

    Feat: 🎉 Added LayoutBox component

---
+++ frontend/src/components/shared/LayoutBox/index.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const LayoutBox = styled.main`
+	background: ${props => props.theme.colors.white};
+	padding: 1.5rem;
+	border-radius: 8px;
+	min-height: 500px;
+`;
