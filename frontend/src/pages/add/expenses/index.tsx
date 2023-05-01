3ad24c307cd9256881d5a146e846d1ca9d665b5f
Guilherme Selair <guilhermeselair@gmail.com>
2023-04-30 21:33:25 -0300

    Chore: 🔨 Continue create expense form

--- frontend/src/pages/add/expenses/index.tsx
+++ frontend/src/pages/add/expenses/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { ArrowSquareOut } from 'phosphor-react';
+import Link from 'next/link';
 
 import { LayoutBox } from '@/components/shared/LayoutBox';
-import { Input } from '@/components/shared/Form/Input';
+import { Input, Select } from '@/components/shared/Form';
 
 import {
 	RegisterExpenseTitle,
 	RegisterExpenseForm,
-} from '@/styles/pages/add/expense.styles';
+	Row,
+	Column,
+	FieldDescription,
+	FieldLabel,
+} from '@/styles/pages/add/expense.style';
 
 export default function CreateExpenses() {
 	return (
@@ -15,16 +21,26 @@ export default function CreateExpenses() {
 
 			<RegisterExpenseForm>
 				<Input label="Nome:" placeholder="Insira o nome de sua despesa aqui" />
-				<div className="inline">
-					<Input
-						label="Nome:"
-						placeholder="Insira o nome de sua despesa aqui"
-					/>
-					<Input
-						label="Nome:"
-						placeholder="Insira o nome de sua despesa aqui"
-					/>
-				</div>
+				<Row margin="0.8rem 1.5rem 0 0">
+					<Column width="418px">
+						<FieldLabel>
+							<span>Meio de pagamento:</span>
+							<Select
+								placeholder="Selecione o meio de pagamento"
+								options={[]}
+							/>
+						</FieldLabel>
+						<FieldDescription>
+							Não encontrou o meio de pagamento?
+							<strong>
+								<Link href={'/add/card'} prefetch={false}>
+									Crie um aqui!
+									<ArrowSquareOut />
+								</Link>
+							</strong>
+						</FieldDescription>
+					</Column>
+				</Row>
 			</RegisterExpenseForm>
 		</LayoutBox>
 	);
