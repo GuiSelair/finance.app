ae074d80ea28764c71f68aabbbc3d6c5ad2ed7b8
Guilherme Selair <guilherme.lima@getrak.com.br>
2024-04-26 19:40:41 -0300

    feat: models transformed to class

---
+++ frontend/src/models/Expense.ts
@@ -0,0 +1,56 @@
+import { parseIsoTODate } from '@/helpers/dateFormat';
+import { formatCurrency } from '@/helpers/formatCurrency';
+
+import { Card } from './Card';
+
+export interface ExpenseProps {
+	id: string;
+	name: string;
+	amount: number;
+	cardId: string;
+	card: Card;
+	userId: string;
+	parcel: number;
+	description?: string;
+	purchaseDate?: string;
+	dueDate?: string;
+	isSplitedExpense?: boolean;
+	isRecurring?: boolean;
+	shareWith?: string;
+	valueOfEach?: string;
+
+	createdAt: string;
+	updatedAt: string;
+}
+
+export class Expense {
+	id: string;
+	name: string;
+	amount: number;
+	isSplitedExpense: boolean;
+	isRecurring: boolean;
+	card: Card;
+	userId: string;
+	parcel: number;
+	description?: string;
+	purchaseDate?: Date;
+	dueDate?: Date;
+	amountFormatted?: string;
+	sharedWith?: string;
+	valueOfEach?: string;
+
+	constructor(data: ExpenseProps) {
+		this.id = data.id;
+		this.name = data.name;
+		this.description = data.description ?? undefined;
+		this.purchaseDate = parseIsoTODate(data.purchaseDate);
+		this.dueDate = parseIsoTODate(data.dueDate);
+		this.amount = Number(data.amount);
+		this.amountFormatted = formatCurrency(Number(data.amount));
+		this.isSplitedExpense = data.isSplitedExpense ?? false;
+		this.isRecurring = data.isRecurring ?? false;
+		this.userId = data.userId;
+		this.card = data.card;
+		this.parcel = Number(data.parcel);
+	}
+}
---
+++ frontend/src/models/ExpenseInMonth.ts
@@ -0,0 +1,43 @@
+import { parseIsoTODate } from '@/helpers/dateFormat';
+
+import { Expense } from './Expense';
+
+export interface ExpenseInMonthProps {
+	id: string;
+	expenseId: string;
+	expense?: Expense;
+	currentParcel: number;
+	quantityParcel: number;
+	valueParcel: number;
+	month: number;
+	year: number;
+	isPaid?: boolean;
+
+	createdAt: string;
+}
+
+export class ExpenseInMonth {
+	id: string;
+	expenseId: string;
+	expense?: Expense;
+	currentParcel: number;
+	quantityParcel: number;
+	valueParcel: number;
+	month: number;
+	year: number;
+	isPaid: boolean;
+	createdAt?: Date;
+
+	constructor(data: ExpenseInMonthProps) {
+		this.id = data.id;
+		this.expenseId = data.expenseId;
+		this.expense = data.expense;
+		this.currentParcel = data.currentParcel;
+		this.quantityParcel = data.quantityParcel;
+		this.valueParcel = Number(data.valueParcel);
+		this.month = Number(data.month);
+		this.year = Number(data.year);
+		this.isPaid = data.isPaid ?? false;
+		this.createdAt = parseIsoTODate(data.createdAt);
+	}
+}
