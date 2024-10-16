9030e89075eba07b8c3db81cac597bcaa6638412
Guilherme <guilherme.selair@gmail.com>
2024-10-16 18:14:08 -0300

    fix: fixed error (Module not found: Can't resolve 'hoist-non-react-statics')

--- apps/web/src/pages/_document.page.tsx
+++ apps/web/src/pages/_document.page.tsx
@@ -45,7 +45,7 @@ export default class MyDocument extends Document {
 					<link
 						rel="preconnect"
 						href="https://fonts.gstatic.com"
-						crossOrigin=''
+						crossOrigin="anonymous"
 					/>
 					<meta charSet="utf-8" />
 
