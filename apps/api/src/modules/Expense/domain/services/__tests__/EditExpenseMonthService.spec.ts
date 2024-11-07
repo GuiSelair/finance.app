3a0cb32f5a2c9021e21300d8142184f1d9f56051
Guilherme Selair <guilherme.selair@gmail.com>
2024-11-07 20:33:26 -0300

    test: added EditExpenseMonthService test

--- apps/api/src/modules/Expense/domain/services/__tests__/EditExpenseMonthService.spec.ts
+++ apps/api/src/modules/Expense/domain/services/__tests__/EditExpenseMonthService.spec.ts
@@ -1,9 +1,13 @@
 import 'reflect-metadata'
 
+import { randomUUID } from 'node:crypto'
+
 import { IExpensesRepository } from '../../repositories/IExpensesRepository'
 import { IExpensesMonthRepository } from '../../repositories/IExpensesInMonthRepository'
 import { EditExpenseMonthService } from '../EditExpenseMonthService'
 import { ExpenseMonthMapper } from '@modules/Expense/infra/typeorm/entities/ExpenseMonthMapper'
+import { ICardsRepository } from '@modules/Card/domain/repositories/ICardsRepository'
+import AppError from '@shared/errors/AppError'
 
 const expensesMonthRepositoryMocked = {
   findById: jest.fn().mockResolvedValue({
@@ -20,39 +24,70 @@ const expensesMonthRepositoryMocked = {
     value_of_parcel: 10,
     expense_id: 'fake-expense-id',
   } as ExpenseMonthMapper),
-}
+  update: jest.fn()
+} as Partial<IExpensesMonthRepository>
 const expensesRepositoryMocked = {
   edit: jest.fn(),
+  update: jest.fn()
+}
+const cardsRepositoryMocked = {
+  findById: jest.fn().mockResolvedValue({ id: 'fake-card-id' })
 }
 
 const editExpenseMonthService = new EditExpenseMonthService(
   expensesRepositoryMocked as unknown as IExpensesRepository,
-  expensesMonthRepositoryMocked as unknown as IExpensesMonthRepository
+  expensesMonthRepositoryMocked as unknown as IExpensesMonthRepository,
+  cardsRepositoryMocked as unknown as ICardsRepository
 )
 
-describe.skip('EditExpenseMonthService use case - Unit test', () => {
+describe('EditExpenseMonthService use case - Unit test', () => {
   it('should be able to edit expense month', async () => {
+    const fakeExpenseMonthUuid = randomUUID()
+    const fakeCardUuid = randomUUID()
     const valuesToChange = {
       is_paid: true,
       name: 'fake-expense-name-changed',
       description: 'fake-description-changed',
       value_of_parcel: 20,
-      card_id: 'fake-card-id-changed',
+      card_id: fakeCardUuid,
     }
 
-    const expenseMonthChangedResponse = await editExpenseMonthService.execute({ id: 'fake-expense-month-id', user_id: 'fake-user-id', valuesToChange })
-    expect(expenseMonthChangedResponse).toEqual(
-      expect.objectContaining({
-        id: 'fake-expense-month-id',
-        expense_id: 'fake-expense-id',
-        is_paid: true,
-        value_of_parcel: 20,
-        expense: expect.objectContaining({
-          name: 'fake-expense-name-changed',
-          description: 'fake-description-changed',
-          card_id: 'fake-card-id-changed',
+    await editExpenseMonthService.execute({ id: fakeExpenseMonthUuid, user_id: randomUUID(), valuesToChange })
+
+    expect(expensesMonthRepositoryMocked.update).toHaveBeenCalledWith({
+      id: fakeExpenseMonthUuid,
+      data:
+        expect.objectContaining({
+          id: fakeExpenseMonthUuid,
+          is_paid: true,
+          value_of_parcel: 20,
         })
+    })
+    expect(expensesRepositoryMocked.update).toHaveBeenCalledWith({
+      id: 'fake-expense-id',
+      data: expect.objectContaining({
+        name: 'fake-expense-name-changed',
+        description: 'fake-description-changed',
+        card_id: fakeCardUuid,
       })
-    )
+    })
+  })
+
+  it('should not be able update card id to unexistable card', async () => {
+    cardsRepositoryMocked.findById.mockResolvedValueOnce(null)
+    const fakeExpenseMonthUuid = randomUUID()
+    const fakeCardUuid = randomUUID()
+    const valuesToChange = {
+      is_paid: true,
+      name: 'fake-expense-name-changed',
+      description: 'fake-description-changed',
+      value_of_parcel: 20,
+      card_id: fakeCardUuid,
+    }
+
+    await expect(
+      editExpenseMonthService.execute({ id: fakeExpenseMonthUuid, user_id: randomUUID(), valuesToChange })
+    ).rejects.toBeInstanceOf(AppError)
+
   })
 })
