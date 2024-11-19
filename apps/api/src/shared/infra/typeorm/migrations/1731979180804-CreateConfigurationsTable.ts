a542a1f45ac04146ec1516d83de38ab51ac886bd
Guilherme <guilherme.selair@gmail.com>
2024-11-18 22:43:43 -0300

    feat: added migration file

---
+++ apps/api/src/shared/infra/typeorm/migrations/1731979180804-CreateConfigurationsTable.ts
@@ -0,0 +1,74 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateConfigurationsTable1731979180804 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: "configurations",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            generationStrategy: "increment"
+          },
+          {
+            name: "key",
+            type: "varchar",
+          },
+          {
+            name: "value",
+            type: "text"
+          },
+          {
+            name: "user_id",
+            type: "uuid"
+          },
+          {
+            name: "month",
+            type: "int",
+            isNullable: true,
+            default: null
+          },
+          {
+            name: "year",
+            type: "int",
+            isNullable: true,
+            default: null
+          },
+          {
+            name: "default",
+            type: "boolean",
+            default: true,
+            isNullable: true
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "configUser",
+            columnNames: ["user_id"],
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            onDelete: "SET NULL",
+            onUpdate: "CASCADE"
+          }
+        ],
+        indices: [
+          {
+            name: 'idx_configurations_user_id_key',
+            columnNames: ['key', 'user_id' ],
+          }
+        ]
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable("configurations")
+    }
+
+}
