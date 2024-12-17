65af339a05a18395ef5005c2cc1f360295ea44d3
Guilherme <guilherme.selair@gmail.com>
2024-12-16 22:46:38 -0300

    fix[web]: fixed scheduler time

--- apps/api/src/shared/infra/scheduler/index.ts
+++ apps/api/src/shared/infra/scheduler/index.ts
@@ -2,9 +2,9 @@ import schedule from 'node-schedule';
 import processRecurringExpenses from './scripts/processRecurringExpenses';
 
 /**
- * Esse script deve ser executado todo dia 15º de cada mês.
+ * Esse script deve ser executado todo dia 15º de cada mês às 00:00.
  */
-const processRecurringExpensesJob = schedule.scheduleJob('* * 15 * *', async () => {
+const processRecurringExpensesJob = schedule.scheduleJob('0 0 15 * *', async () => {
   await processRecurringExpenses();
 });
 
