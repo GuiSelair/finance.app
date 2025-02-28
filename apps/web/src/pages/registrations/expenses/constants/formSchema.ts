09ae093068e5f2ae868b5c1e43f176ade18c9fac
Guilherme Selair <guilherme.selair@gmail.com>
2025-02-27 22:48:14 -0300

    fix[FA-20][web]: fixed error message to expense value field

--- apps/web/src/pages/registrations/expenses/constants/formSchema.ts
+++ apps/web/src/pages/registrations/expenses/constants/formSchema.ts
@@ -8,7 +8,9 @@ export const createFormExpenseFormSchema = (isEditMode = false) => {
 		purchaseDate: Yup.string().required(requiredFieldMessage),
 		manualExpenseDate: isEditMode ? Yup.string() : Yup.string().required(requiredFieldMessage),
 		category: Yup.string(),
-		totalValue: Yup.number().required(requiredFieldMessage),
+		totalValue: Yup.number()
+			.required(requiredFieldMessage)
+			.transform((_, originalValue) => Number(originalValue.replace(',', '.'))),
 		parcelQuantity: Yup.number().required(requiredFieldMessage),
 		paymentMethod: Yup.object()
 			.shape({
