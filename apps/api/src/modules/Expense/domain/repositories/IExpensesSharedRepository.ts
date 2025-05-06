1715cf9aeab1ebcce7dd56b3b65521788384443c
Guilherme Selair <guilherme.selair@gmail.com>
2025-05-05 23:49:05 -0300

    feat[api][FA-24]: add fetchByExpenseMonthIds method to IExpensesSharedRepository and implement it in ExpensesSharedRepository; update FetchBalanceMonthService to utilize this method for calculating total payable expenses

--- apps/api/src/modules/Expense/domain/repositories/IExpensesSharedRepository.ts
+++ apps/api/src/modules/Expense/domain/repositories/IExpensesSharedRepository.ts
@@ -2,4 +2,5 @@ import { ExpenseShared } from "../models/ExpenseShared";
 
 export interface IExpensesSharedRepository {
   create(args: ExpenseShared[]): Promise<void>;
+  fetchByExpenseMonthIds(expense_month_ids: string[]): Promise<ExpenseShared[]>;
 }
--- apps/api/src/modules/Expense/domain/services/__tests__/FetchBalanceMonthService.spec.ts
+++ apps/api/src/modules/Expense/domain/services/__tests__/FetchBalanceMonthService.spec.ts
@@ -1,9 +1,11 @@
 import { FetchBalanceMonthService } from '../FetchBalanceMonthService';
 import { IExpensesMonthRepository } from '../../repositories/IExpensesMonthRepository';
 import { IIncomesRepository } from '@modules/Settings/domain/repositories/IIncomeRepository';
+import { IExpensesSharedRepository } from '../../repositories/IExpensesSharedRepository';
 
 let expensesMonthRepositoryMocked: Partial<IExpensesMonthRepository>
 let incomesRepositoryMocked: Partial<IIncomesRepository>
+let expensesSharedRepositoryMocked: Partial<IExpensesSharedRepository>
 let fetchBalanceMonthService: FetchBalanceMonthService
 
 describe('GetBalanceOfMonthService use case - Unit test', () => {
@@ -22,10 +24,17 @@ describe('GetBalanceOfMonthService use case - Unit test', () => {
     incomesRepositoryMocked = {
       findByMonthAndYear: jest.fn().mockResolvedValue({ value: 1000, month: 0, year: 2024 })
     }
+    expensesSharedRepositoryMocked = {
+      fetchByExpenseMonthIds: jest.fn().mockResolvedValue([
+        { amount: 10 },
+        { amount: 20 },
+      ])
+    }
 
     fetchBalanceMonthService = new FetchBalanceMonthService(
       expensesMonthRepositoryMocked as unknown as IExpensesMonthRepository,
-      incomesRepositoryMocked as unknown as IIncomesRepository
+      incomesRepositoryMocked as unknown as IIncomesRepository,
+      expensesSharedRepositoryMocked as unknown as IExpensesSharedRepository
     );
   })
 
@@ -37,9 +46,9 @@ describe('GetBalanceOfMonthService use case - Unit test', () => {
     });
 
     expect(balanceMonthServiceOutput).toEqual({
-      economy: 850.0,
+      economy: 880.0,
       totalOfExpenses: 150.0,
-      totalPayable: 150.0,
+      totalPayable: 120.0,
     });
   });
 });
