cb9a1663cbe3f30e44286f03a3c2ddd446a3f953
Guilherme Selair <guiherme.selair@gmail.com>
2025-06-14 22:21:03 -0300

    feat[FA-30][web]: added whatsapp button with expense details values

--- apps/web/src/pages/dividers/total-per-person/hooks/useTotalPerPerson.tsx
+++ apps/web/src/pages/dividers/total-per-person/hooks/useTotalPerPerson.tsx
@@ -1,24 +1,26 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { toast } from 'react-toastify';
 import { WhatsappLogo } from 'phosphor-react';
 
-import { Avatar, Button, Flex } from '@/components';
+import { Avatar, Button, Flex, Spinner } from '@/components';
 import { useFetchTotalPerPersonApi } from '@/hooks/api/sharePeople/useFetchTotalPerPerson.api';
-import { SharedExpense } from '@/models/SharedExpense';
 import { useContextSelector } from 'use-context-selector';
 import { selectedMonthYearContext } from '@/contexts/SelectedMonthYearContext';
+import { useFetchSharedExpensesByPersonApi } from '@/hooks/api/sharePeople/useFetchSharedExpenseslPerPerson.api';
+import { formatCurrency } from '@/helpers/formatCurrency';
+import { formatParcel } from '@/helpers/formatParcel';
 
 export function useTotalPerPerson() {
 	const month = useContextSelector(selectedMonthYearContext, state => state.month);
 	const year = useContextSelector(selectedMonthYearContext, state => state.year);
+	const [selectedPersonId, setSelectedPersonId] = useState<number | null>(null);
 	const { data: totalPerPersonListResponse, isLoading: isLoadingTotalPerPerson, isError } = useFetchTotalPerPersonApi();
+	const { data: sharedExpensesByPerson, isLoading: isLoadingSharedExpensesByPerson } =
+		useFetchSharedExpensesByPersonApi(selectedPersonId, { enabled: !!selectedPersonId });
 
 	const totalPerPersonList = useMemo(() => {
 		return (
 			totalPerPersonListResponse?.map(totalPerPerson => {
-				const whatsappLink = `https://wa.me/55${totalPerPerson.person.whatsapp}?text=${makeWhatsappTemplate(
-					totalPerPerson,
-				)}`;
 				return {
 					avatar: <Avatar name={totalPerPerson.person.name} />,
 					id: totalPerPerson.person.id,
@@ -27,31 +29,41 @@ export function useTotalPerPerson() {
 					total: totalPerPerson.getTotalFormatted(),
 					actions: (
 						<Flex padding="0 4px">
-							<Button variant="ghost" size="icon" onClick={() => window.open(whatsappLink, '_blank')}>
-								<WhatsappLogo weight="light" size={18} />
+							<Button variant="ghost" size="icon" onClick={() => setSelectedPersonId(totalPerPerson.person.id)}>
+								{isLoadingSharedExpensesByPerson ? <Spinner size="sm" /> : <WhatsappLogo weight="light" size={18} />}
 							</Button>
 						</Flex>
 					),
 				};
 			}) || []
 		);
-	}, [totalPerPersonListResponse]);
+	}, [totalPerPersonListResponse, isLoadingSharedExpensesByPerson]);
 
-	function makeWhatsappTemplate(totalPerPerson: SharedExpense) {
-		return `
+	function makeWhatsappTemplate(personId: number) {
+		const sharedExpenseByPerson = totalPerPersonListResponse?.find(expense => expense.person.id === personId);
+		const whatsappLink = `https://wa.me/55${sharedExpenseByPerson?.person.whatsapp}`;
+
+		const template = `
 			Sua conta chegou!
 
-			Valor total:  *${totalPerPerson.getTotalFormatted()}*
+			Valor total:  *${sharedExpenseByPerson?.getTotalFormatted()}*
 
 			_Detalhamento:_
-			_1. [Despesa 1][parcelas]: R$ ${totalPerPerson.getTotalFormatted()}_
+			${sharedExpensesByPerson?.expensesSharedDetails
+				.map(
+					(expense, index) =>
+						`_${index + 1}. ${expense.name}[${formatParcel(
+							expense.currentParcel,
+							expense.totalParcel,
+						)}]: R$ ${formatCurrency(expense.amount)}`,
+				)
+				.join('\n')}
 
 			---------------
-
-			_Pix para pagamento: [Chave pix]_
-
 			_Referente ao ${month}/${year}_
 		`;
+
+		window.open(`${whatsappLink}?text=${template}`, '_blank');
 	}
 
 	const isEmptyState = !totalPerPersonList?.length && !isLoadingTotalPerPerson;
@@ -64,9 +76,17 @@ export function useTotalPerPerson() {
 		}
 	}, [isError]);
 
+	/** Effect responsável por abrir o whatsapp com o template de compartilhamento de despesas assim que os dados do detalhamento das despesas forem carregados. */
+	useEffect(() => {
+		if (sharedExpensesByPerson && selectedPersonId) {
+			makeWhatsappTemplate(selectedPersonId);
+		}
+	}, [sharedExpensesByPerson]);
+
 	return {
 		totalPerPersonList,
 		isEmptyState,
 		isLoadingTotalPerPerson,
+		isLoadingSharedExpensesByPerson,
 	};
 }
