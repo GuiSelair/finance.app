b25d61f69bb5b7c60356425f8d46265963c1284c
Guilherme Selair <guilherme.selair@gmail.com>
2025-06-15 20:38:58 -0300

    feature/FA-29 - Implementação do dropmenu responsável deslogar usuário (#101)
    feat[FA-29][web]: added dropmenu component and sign-out dropmenu button
    
    Co-authored-by: Guilherme Selair <guiherme.selair@gmail.com>

---
+++ apps/web/src/components/Dropmenu/index.ts
@@ -0,0 +1,9 @@
+import * as RadixDropmenu from '@radix-ui/react-dropdown-menu';
+
+import { DropmenuContent, DropmenuItem } from './Dropmenu';
+
+export const Dropmenu = {
+	...RadixDropmenu,
+	Content: DropmenuContent,
+	Item: DropmenuItem,
+};
